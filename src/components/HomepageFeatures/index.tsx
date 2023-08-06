e4dd3613e7f15d856bd88c1b3fa03269728d6f50
Rahul Deshar <desarrahul@gmail.com>
2023-08-06 23:38:05 +0545

    Automatic backup: 2023-08-06 23:38:05

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  description: JSX.Element;
+};
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container"></div>
+    </section>
+  );
+}
